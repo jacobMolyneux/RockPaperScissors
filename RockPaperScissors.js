cbd96d1f28f1d292154a25db45b8fa0d69e2da87
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-05-12 18:00:01 -0400

    added buttons
    Added buttons to the program so user can choose their option for RPS

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,3 +1,6 @@
+
+
+
 function computerPlay(){
         i = Math.random();
         
@@ -11,7 +14,9 @@ function computerPlay(){
         else{
             return "scissors";
         }
-    }
+  }
+let playerScore = 0;
+let computerScore = 0;
 function playRound(PlayerSelection, computerPlay){
     let computerChoice = computerPlay;
     let player = PlayerSelection.toLowerCase();
@@ -19,30 +24,62 @@ function playRound(PlayerSelection, computerPlay){
     console.log("You Chose :" + player);
     if(player == 'rock' && computerChoice == 'scissors'){
         console.log('You Win!');
+        playerScore++;
     }
     else if(player == 'rock' && computerChoice == 'paper'){
         console.log('You Lose!');
+        computerScore++;
     }
     else if(player == 'paper' && computerChoice == 'rock'){
         console.log("You Win!");
+        playerScore++;
     }
     else if(player == 'paper' && computerChoice == 'scissors'){
         console.log('You Lose!');
+        computerScore++;
     }
     else if(player == 'scissors' && computerChoice == 'rock'){
         console.log('You Lose!');
+        computerScore++;
     }
     else if(player == 'scissors' && computerChoice == 'paper'){
         console.log('You Win!');
+        playerScore++;
     }
     else if(player == computerChoice){
         console.log("Its a Tie!");
     }
     
 }
-let PlayerSelection = prompt("what is your choice?")
-console.log("You chose " + PlayerSelection)
-computerPlay()
-playRound(PlayerSelection, computerPlay())
 
+
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+
+rock.addEventListener("click", rockClicked);
+function rockClicked(){
+    let PlayerSelection = 'rock'
+    console.log("You chose " + PlayerSelection)
+    computerPlay()
+    playRound(PlayerSelection, computerPlay())
+}
+paper.addEventListener('click', paperClicked);
+function paperClicked(){
+    let PlayerSelection = 'paper';
+    console.log("You chose " + PlayerSelection)
+    computerPlay()
+    playRound(PlayerSelection, computerPlay())
+}
+scissors.addEventListener("click", scissorsClicked);
+function scissorsClicked(){
+    let PlayerSelection = 'scissors';
+    console.log("You chose " + PlayerSelection)
+    computerPlay()
+    playRound(PlayerSelection, computerPlay())
+}
+let pScore = document.getElementById('pScore');
+let cScore = document.getElementById('cScore');
+pScore.textContent = str(playerScore);
+cScore.textContent = str(computerScore);
 
